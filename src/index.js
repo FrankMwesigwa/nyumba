ba7958a2d6e27f305f8ab25a517d672806f69b8e
Frank Mwesigwa <frank.mwesigwa1@gmail.com>
2019-05-16 01:25:50 +0300

    setup redux for global state management

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import { Provider } from "react-redux";
+import store from "./store";
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+document.getElementById('root')
+);
