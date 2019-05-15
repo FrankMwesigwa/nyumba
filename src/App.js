be42a38d65298036398b9470c51a2cf32bcca3f3
Frank Mwesigwa <frank.mwesigwa1@gmail.com>
2019-05-16 01:54:06 +0300

    link redux to register and login component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React , { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 
 import Navbar from "./layout/Navbar";
 import Landing from "./layout/Landing";
 
 import Register from "./auth/Register";
 import Login from "./auth/Login";
+
+import PrivateRoute from "./components/private-route/PrivateRoute";
+import Dashboard from "./components/dashboard/Dashboard";
+
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+// Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Redirect to login
+    window.location.href = "./login";
+  }
+}
 
 class App extends Component {
 
@@ -17,6 +42,9 @@ class App extends Component {
           <Route exact path="/" component={Landing} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/login" component={Login} />
+          <Switch>
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
